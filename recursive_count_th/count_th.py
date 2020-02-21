b3451be6e990c4bf201a378fac95703237529b25
James Grantham <james.r.grantham@gmail.com>
2020-02-21 09:32:12 +0000

    /usr/local/opt/python/bin/python3.7 /Users/jamesgrantham/Documents/LambdaProjects/19-Algorithms/Sprint-Challenge--Algorithms/recursive_count_th/test_count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,12 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    if len(word) < 2:
+        return 0
+
+    if word[:2] == 'th':
+        return 1 + count_th(word[1:])
+    else:
+        return count_th(word[1:])
+
+print(count_th('theth'))
