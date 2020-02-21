3dcfe02641c09a8328bbd0979aaee98dce062f17
James Grantham <james.r.grantham@gmail.com>
2020-02-21 09:33:36 +0000

    th count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,12 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
+    # define base case
     if len(word) < 2:
         return 0
 
+    # check the first 2 characters for match 'th'
     if word[:2] == 'th':
+        # if match return 1 and feed the recursion removing the first letter
         return 1 + count_th(word[1:])
     else:
+        # if not match feed the recursion removing the first letter
         return count_th(word[1:])
 
 print(count_th('theth'))
